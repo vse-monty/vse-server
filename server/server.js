25d3f9d328fecd2cc2085e3d0dba6e5d7dabddaf
vse-monty <49755895+vse-monty@users.noreply.github.com>
2019-07-16 09:59:50 -0500

    added order listener

--- server/server.js
+++ server/server.js
@@ -6,20 +6,29 @@ app.get('/', function(req, res){
     res.send('<p>express server running...</p>');
 });
 
+var socketList = [];
+
 io.on('connection', function(socket){
     console.log('SERVER: a user connected: ' + socket.id);
+    socketList.push(socket);
 
     socket.on('get.variables', function(data){
         console.log('SERVER: data sent to panel!')
         console.log(data);
         socket.broadcast.emit('get.variables', data);
-    })
+    });
     
     socket.on('give.variables', function(data){
         console.log('SERVER: data sent to app!')
         console.log(JSON.parse(data));
         socket.broadcast.emit('give.variables', data);
-    })
+    });
+    
+    socket.on('order', function(data){
+        console.log('SERVER: sending order data to panel!')
+        console.log(JSON.parse(data));
+        socket.broadcast.emit('process.order', data);
+    });
 });
 
 io.on('disconnect', (socket) =>{
